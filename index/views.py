87b272fdada7b5d1f43f4503dad06c607dcdf624
hanrimJO <whgksfla@gmail.com>
2020-10-12 16:15:00 +0900

    fix Jenkinsfile with deploy step47

--- index/views.py
+++ index/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def first(request):
-    return HttpResponse('oweijfoweijfoijoiewjfoiwejfoweijfoijofijaofijasldfkjㅐㅑ멀ㅇ니ㅏ러ㅐㅈ니ㅑ더리나러ㅣㄴ다ㅑㅓ')
+    return HttpResponse('ijoijoij')
